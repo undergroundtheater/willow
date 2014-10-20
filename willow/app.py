7bf45c368de88b85ea297c82798cab668cbe0f77
Michael King <mking@arikel.net>
2014-10-20 13:57:36 +0000

    initial structure

---
+++ willow/app.py
@@ -0,0 +1,35 @@
+import os
+from blinker import Namespace
+from flask import Flask
+from flask_wtf import CsrfProtect
+from werkzeug.utils import import_string
+
+willow_signals = Namespace()
+
+def create_app():
+    app = Flask(__name__, static_folder='public')
+    
+    app.environment = os.getenv('WILLOW_ENV', 'Dev')
+
+    if app.environment != 'Test':
+        CsrfProtect(app)
+
+    app.config.from_object('willow.settings.{}Config'.format(app.environment))
+
+    # import plugins
+
+    app.loaded_plugins = {}
+    app.admin_user_hooks = []
+    app.dashboard_hooks = []
+    app.character_hooks = []
+    app.navbar = {'admin': [], 'extra': []}
+
+    for plugin in app.config['PLUGINS']:
+        imported_plugin = import_string(plugin)()
+        imported_plugin.init_app(app)
+        app.loaded_plugins[plugin] = imported_plugin
+
+    # import blueprints
+    # Note that plugins should do this automatically
+    # UserView.register(app)
+    # CharacterView.register(app)
