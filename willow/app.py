137c89b33a9707e82e42b0360faac4611f6cb1b7
Drew Stevens <wastevens@gmail.com>
2014-11-12 18:30:18 -0500

    Resolved compile error; willows.settings did not exist, so I changed it to willows.settings_dist (which did exist), and lo, compilation.

--- willow/app.py
+++ willow/app.py
@@ -19,7 +19,7 @@ def create_app():
     if app.environment != 'Test':
         CsrfProtect(app)
 
-    app.config.from_object('willow.settings.{}Config'.format(app.environment))
+    app.config.from_object('willow.settings_dist.{}Config'.format(app.environment))
 
     from willow.models import db, user_datastore
     from flask.ext.security import Security
